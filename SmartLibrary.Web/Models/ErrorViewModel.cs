62bd38a35be3fec85fc83b9d092dd5487fc17ca6
Mohamed Agwa <magwa912@gmail.com>
2025-10-19 07:10:22 +0300

    Add project files.

---
+++ SmartLibrary.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartLibrary.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
