9421d4e215645f5141b9bb14522ce02f171bb4c5
Mohamed Agwa <magwa912@gmail.com>
2025-10-23 12:40:46 +0300

    Enhance Layout through render Create/Edit Form to bootstrap Modal

--- SmartLibrary.Web/Controllers/CategoriesController.cs
+++ SmartLibrary.Web/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SmartLibrary.Web.Core.Models;
+using SmartLibrary.Web.Filters;
 
 namespace SmartLibrary.Web.Controllers
 {
@@ -21,6 +22,7 @@ namespace SmartLibrary.Web.Controllers
         }
 
         [HttpGet]
+        [AjaxOnly]
         public IActionResult Create()
         {
             return PartialView("_Form");
@@ -31,19 +33,21 @@ namespace SmartLibrary.Web.Controllers
         public IActionResult Create(CategoryViewModel model)
         {
             if (!ModelState.IsValid)
-                return View("_Form" ,model);
-            var Category = new Category()
+                return BadRequest();
+            var category = new Category()
             {
                 Name = model.Name
             };
-            _context.Add(Category);
+            _context.Add(category);
             _context.SaveChanges();
             //Send Alerts Controllers to Views.
-            TempData["Message"] = "Saved Successfully";
-            return RedirectToAction(nameof(Index));
+            //TempData["Message"] = "Saved Successfully";
+            //return RedirectToAction(nameof(Index));
+            return PartialView("_CategoryRow", category);
         }
 
         [HttpGet]
+        [AjaxOnly]
         public IActionResult Edit(int id)
         {
             var category = _context.Categories.Find(id);
@@ -55,7 +59,7 @@ namespace SmartLibrary.Web.Controllers
                 Name = category.Name
                 
             };
-            return View("_Form", viewModel);
+            return PartialView("_Form", viewModel);
         }
 
         [HttpPost]
@@ -63,15 +67,15 @@ namespace SmartLibrary.Web.Controllers
         public IActionResult Edit(CategoryViewModel model)
         {
             if (!ModelState.IsValid)
-                return View("_Form", model);
+                BadRequest();
             var category = _context.Categories.Find(model.Id);
             if (category is null)
                 return NotFound();
             category.Name = model.Name;
             category.LastUpdatedOn = DateTime.Now;
             _context.SaveChanges();
-            TempData["Message"] = "Saved Successfully";
-            return RedirectToAction(nameof(Index));
+            return PartialView("_CategoryRow", category);
+
         }
 
 
--- SmartLibrary.Web/wwwroot/js/site.js
+++ SmartLibrary.Web/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿function ShowSuccessMessage(message = "Saved Successfully") {
+﻿var UpdatedRow;
+function ShowSuccessMessage(message = "Saved Successfully") {
     Swal.fire({
         icon: "success",
         title: "success",
@@ -19,6 +20,18 @@ function ShowErrorMessage(message = "Something went wrong!") {
     });
 }
 
+function OnModalSuccess(item) {
+    ShowSuccessMessage();
+    $('#Modal').modal('hide');
+    if (UpdatedRow === undefined) {
+        $('tbody').append(item);
+    } else {
+        $(UpdatedRow).replaceWith(item);
+        UpdatedRow = undefined;
+    }
+    KTMenu.init();
+    KTMenu.initHandlers();
+}
 $(document).ready(function () {
     var message = $('#Message').text();
     if (message !== '') {
@@ -27,16 +40,20 @@ $(document).ready(function () {
 
 
     //handle bootstrap modal
-    $('.js-render-modal').on('click', function () {
+    $('body').delegate('.js-render-modal' ,'click', function () {
         var btn = $(this);
         var modal = $('#Modal');
         modal.find('#ModalLabel').text(btn.data('title'));
-
+        if (btn.data('update') !== undefined) {
+            UpdatedRow = btn.parents('tr');
+            //console.log(UpdatedRow);
+        }
         $.get({
             url: btn.data('url'),
             success: function (form) {
-                console.log(form);
+                //console.log(form);
                 modal.find('.modal-body').html(form);
+                $.validator.unobtrusive.parse(modal);
             },
             error: function () {
                 ShowErrorMessage();
